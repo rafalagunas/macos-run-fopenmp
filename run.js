ec2c2b238ea093817e3ba1759c1be2a9fb8e04ac
Rafael Lagunas <rafalagunas@MacBook-Pro-de-Rafael.local>
2020-08-24 21:25:22 -0500

    first commit

---
+++ run.js
@@ -0,0 +1,16 @@
+const { exec } = require("child_process");
+
+var myArgs = process.argv.slice(2);
+console.log(myArgs.toString());
+
+exec("g++-10 -fopenmp -o your_program " + myArgs, (error, stdout, stderr) => {
+  if (error) {
+    console.log(`error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.log(`stderr: ${stderr}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+});
