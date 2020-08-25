4bae46bf0f911524d7bc1a69db2cb05b9e0c6f6a
Rafael Lagunas <rafalagunas@MacBook-Pro-de-Rafael.local>
2020-08-24 21:30:27 -0500

    [ADD] -mavf flag to exec node.js script

--- run.js
+++ run.js
@@ -3,14 +3,17 @@ const { exec } = require("child_process");
 var myArgs = process.argv.slice(2);
 console.log(myArgs.toString());
 
-exec("g++-10 -fopenmp -o your_program " + myArgs, (error, stdout, stderr) => {
-  if (error) {
-    console.log(`error: ${error.message}`);
-    return;
+exec(
+  "g++-10 -fopenmp -o your_program -mavx " + myArgs,
+  (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
   }
-  if (stderr) {
-    console.log(`stderr: ${stderr}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-});
+);
